7bc9b2f7d90ccf8be024234189a5744fa38926fc
a1dutch <aholland@a1dutch.co.uk>
2014-10-27 20:00:20 +0000

    Added format, command bindings and made open fitness type available via the window

--- fitedit/src/fitedit/editors/FitSourceViewerConfiguration.java
+++ fitedit/src/fitedit/editors/FitSourceViewerConfiguration.java
@@ -10,7 +10,6 @@ import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
-
 import fitedit.editors.syntaxrules.ColorManager;
 import fitedit.editors.syntaxrules.FitScanner;
 import fitedit.editors.syntaxrules.FitSourcePartitionScanner;
@@ -18,56 +17,52 @@ import fitedit.editors.syntaxrules.IFitColorConstants;
 import fitedit.editors.syntaxrules.NonRuleBasedDamagerRepairer;
 
 public class FitSourceViewerConfiguration extends SourceViewerConfiguration {
-	private static FitScanner fitScanner = null;
-	private static ColorManager colorManager = new ColorManager();
-
-	@Override
-	public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
-		return new String[] {
-			FitSourcePartitionScanner.FIT_COMMENT,
-			};
-	}
-	
-	protected FitScanner getFitScanner() {
-		if (fitScanner == null) {
-			fitScanner = new FitScanner(colorManager);
-			fitScanner.setDefaultReturnToken(
-				new Token(
-					new TextAttribute(
-						colorManager.getColor(IFitColorConstants.DEFAULT))));
-		}
-		return fitScanner;
-	}
-
-	@Override
-	public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
-		PresentationReconciler reconciler = new PresentationReconciler();
-
-		DefaultDamagerRepairer dr =
-			new DefaultDamagerRepairer(getFitScanner());
-		reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
-		reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
-
-		NonRuleBasedDamagerRepairer ndr =
-			new NonRuleBasedDamagerRepairer(
-				new TextAttribute(
-					colorManager.getColor(IFitColorConstants.COMMENT)));
-		reconciler.setDamager(ndr, FitSourcePartitionScanner.FIT_COMMENT);
-		reconciler.setRepairer(ndr, FitSourcePartitionScanner.FIT_COMMENT);
-
-		return reconciler;
-	}
-	
-	@Override
-	public IContentFormatter getContentFormatter(ISourceViewer sourceViewer) {
-		MultiPassContentFormatter formatter= 
-			new MultiPassContentFormatter(
-					getConfiguredDocumentPartitioning(sourceViewer), 
-					IDocument.DEFAULT_CONTENT_TYPE);
-		
-		formatter.setMasterStrategy(new FitnesseFormattingStrategy());
-		
-		return formatter;
-	}
-
+    private static FitScanner fitScanner = null;
+    private static ColorManager colorManager = new ColorManager();
+    
+    @Override
+    public String[] getDefaultPrefixes(ISourceViewer sourceViewer, String contentType) {
+        return new String[] { "#" };
+    }
+    
+    @Override
+    public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
+        return new String[] { FitSourcePartitionScanner.FIT_COMMENT, IDocument.DEFAULT_CONTENT_TYPE };
+    }
+    
+    protected FitScanner getFitScanner() {
+        if (fitScanner == null) {
+            fitScanner = new FitScanner(colorManager);
+            fitScanner.setDefaultReturnToken(new Token(new TextAttribute(colorManager
+                    .getColor(IFitColorConstants.DEFAULT))));
+        }
+        return fitScanner;
+    }
+    
+    @Override
+    public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
+        PresentationReconciler reconciler = new PresentationReconciler();
+        
+        DefaultDamagerRepairer dr = new DefaultDamagerRepairer(getFitScanner());
+        reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
+        reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
+        
+        NonRuleBasedDamagerRepairer ndr = new NonRuleBasedDamagerRepairer(new TextAttribute(colorManager
+                .getColor(IFitColorConstants.COMMENT)));
+        reconciler.setDamager(ndr, FitSourcePartitionScanner.FIT_COMMENT);
+        reconciler.setRepairer(ndr, FitSourcePartitionScanner.FIT_COMMENT);
+        
+        return reconciler;
+    }
+    
+    @Override
+    public IContentFormatter getContentFormatter(ISourceViewer sourceViewer) {
+        MultiPassContentFormatter formatter = new MultiPassContentFormatter(
+                getConfiguredDocumentPartitioning(sourceViewer), IDocument.DEFAULT_CONTENT_TYPE);
+        
+        formatter.setMasterStrategy(new FitnesseFormattingStrategy());
+        
+        return formatter;
+    }
+    
 }
--- fitedit/src/fitedit/editors/FitnesseEditor.java
+++ fitedit/src/fitedit/editors/FitnesseEditor.java
@@ -1,5 +1,6 @@
 package fitedit.editors;
 
+import java.util.ResourceBundle;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -14,9 +15,8 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.ide.ResourceUtil;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
-
+import fitedit.editors.actions.CommentTextEditorAction;
 import fitedit.editors.outline.FitOutlinePage;
-
 
 /**
  * Entry point of this plugin
@@ -24,96 +24,108 @@ import fitedit.editors.outline.FitOutlinePage;
  * @author Toshiyuki Fukuzawa
  */
 public class FitnesseEditor extends TextEditor {
-
-	private FitOutlinePage outlinePage;
-	private ProjectionSupport projectionSupport;
-
-	public FitnesseEditor() {
-		super();
-		setSourceViewerConfiguration(new FitSourceViewerConfiguration());
-		setDocumentProvider(new FitDocumentProvider());
-	}
-
-	@Override
-	public Object getAdapter(Class required) {
-
-		// set outline page
-		if (IContentOutlinePage.class.equals(required)) {
-			if (outlinePage == null) {
-				outlinePage = new FitOutlinePage(getDocumentProvider(), this);
-				outlinePage.setInput(getEditorInput());
-			}
-			return outlinePage;
-		}
-
-		// folding
-		if (projectionSupport != null) {
-			Object adapter = projectionSupport.getAdapter(getSourceViewer(), required);
-			if (adapter != null)
-				return adapter;
-		}
-		
-		return super.getAdapter(required);
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-		super.init(site, input);
-		setTabLabel(input);
-	}
-	
-	/**
-	 * Set the tab label with the parent directory's name
-	 */
-	private void setTabLabel(IEditorInput input){
-		IFile file = ResourceUtil.getFile(input);
-		if(file == null){
-			return;
-		}
-		
-		String fileName = file.getName();
-		if(fileName == null || !fileName.equals("content.txt")) {
-			return;
-		}
-		
-		IContainer parent = file.getParent();
-		if(parent == null){
-			return;
-		}
-		
-		setPartName(parent.getName());
-	}
-	
-	@Override
-	public void doSave(IProgressMonitor progressMonitor) {
-		super.doSave(progressMonitor);
-		
-		if(outlinePage != null) {
-			outlinePage.update();
-		}
-	}
-		
-	@Override
-	public void createPartControl(Composite parent) {
-		super.createPartControl(parent);
-
-	    //turn projection mode on
-		ProjectionViewer viewer =(ProjectionViewer)getSourceViewer();
-	    projectionSupport = new ProjectionSupport(viewer,getAnnotationAccess(),getSharedColors());
-	    projectionSupport.install();
-	    viewer.doOperation(ProjectionViewer.TOGGLE);
-	}
-	
-	@Override
-	protected ISourceViewer createSourceViewer(Composite parent,
-			IVerticalRuler ruler, int styles) {
-		ISourceViewer viewer = new ProjectionViewer(parent, ruler,
-				getOverviewRuler(), isOverviewRulerVisible(), styles);
-
-		// ensure decoration support has been created and configured.
-		getSourceViewerDecorationSupport(viewer);
-
-		return viewer;
-	}
+    
+    private FitOutlinePage outlinePage;
+    private ProjectionSupport projectionSupport;
+    
+    public FitnesseEditor() {
+        super();
+        setSourceViewerConfiguration(new FitSourceViewerConfiguration());
+        setDocumentProvider(new FitDocumentProvider());
+    }
+    
+    @Override
+    protected void createActions() {
+        super.createActions();
+        
+        CommentTextEditorAction action = new CommentTextEditorAction(ResourceBundle.getBundle("messages"), "", this);
+        action.configure(getSourceViewer(), getSourceViewerConfiguration());
+        setAction("comment", action);
+    }
+    
+    @Override
+    public Object getAdapter(Class required) {
+        
+        // set outline page
+        if (IContentOutlinePage.class.equals(required)) {
+            if (outlinePage == null) {
+                outlinePage = new FitOutlinePage(getDocumentProvider(), this);
+                outlinePage.setInput(getEditorInput());
+            }
+            return outlinePage;
+        }
+        
+        // folding
+        if (projectionSupport != null) {
+            Object adapter = projectionSupport.getAdapter(getSourceViewer(), required);
+            if (adapter != null)
+                return adapter;
+        }
+        
+        return super.getAdapter(required);
+    }
+    
+    @Override
+    public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+        super.init(site, input);
+        setTabLabel(input);
+    }
+    
+    /**
+     * Set the tab label with the parent directory's name
+     */
+    private void setTabLabel(IEditorInput input) {
+        IFile file = ResourceUtil.getFile(input);
+        if (file == null) {
+            return;
+        }
+        
+        String fileName = file.getName();
+        if (fileName == null || !fileName.equals("content.txt")) {
+            return;
+        }
+        
+        IContainer parent = file.getParent();
+        if (parent == null) {
+            return;
+        }
+        
+        setPartName(parent.getName());
+    }
+    
+    @Override
+    public void doSave(IProgressMonitor progressMonitor) {
+        super.doSave(progressMonitor);
+        
+        if (outlinePage != null) {
+            outlinePage.update();
+        }
+    }
+    
+    @Override
+    public void createPartControl(Composite parent) {
+        super.createPartControl(parent);
+        
+        // turn projection mode on
+        ProjectionViewer viewer = (ProjectionViewer) getSourceViewer();
+        projectionSupport = new ProjectionSupport(viewer, getAnnotationAccess(), getSharedColors());
+        projectionSupport.install();
+        viewer.doOperation(ProjectionViewer.TOGGLE);
+    }
+    
+    @Override
+    protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
+        ISourceViewer viewer = new ProjectionViewer(parent, ruler, getOverviewRuler(), isOverviewRulerVisible(),
+                styles);
+        
+        // ensure decoration support has been created and configured.
+        getSourceViewerDecorationSupport(viewer);
+        
+        return viewer;
+    }
+    
+    @Override
+    protected void initializeKeyBindingScopes() {
+        setKeyBindingScopes(new String[] { "fitedit.editors.fitnesseEditorScope" });
+    }
 }

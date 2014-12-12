8e7f33eb100392d0081468aca5d9d624af6ffd10
a1dutch <aholland@a1dutch.co.uk>
2014-12-12 23:49:30 +0000

    removed submodule and reworked update script

--- update-nightly.sh
+++ update-nightly.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
-rm -rf fitnesseclipse.website/nightly/*
-cp -r fitnesseclipse.site/target/repository/* fitnesseclipse.website/nightly/
-git commit --all -m "Update Nightly"
+git clone git@github.com:fitnesse-eclipse/fitnesse-eclipse.github.io.git
+
+cd fitnesse-eclipse.github.io
+git config user.email "fitnesseclipse@a1dutch.co.uk"
+git config user.name "fitnesseclipse"
+
+rm -rf nightly/*
+cp -r ~/fitnesse-eclipse/fitnesseclipse.site/target/repository/* nightly/
+git commit -a -u -m "Update Nightly"
+git push origin master

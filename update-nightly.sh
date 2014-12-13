301747410a9587c05c7c2911de59b35c546b7cf8
a1dutch <aholland@a1dutch.co.uk>
2014-12-13 00:11:32 +0000

    update nightly script

--- update-nightly.sh
+++ update-nightly.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
-git clone git@github.com:fitnesse-eclipse/fitnesse-eclipse.github.io.git
 
+echo checking out website
+git clone git@github.com:fitnesse-eclipse/fitnesse-eclipse.github.io.git
 cd fitnesse-eclipse.github.io
 git config user.email "fitnesseclipse@a1dutch.co.uk"
 git config user.name "fitnesseclipse"
 
+echo clearing nightly folder
 rm -rf nightly/*
+
+echo updating nightly folder
 cp -r ~/fitnesse-eclipse/fitnesseclipse.site/target/repository/* nightly/
-git commit -a -u -m "Update Nightly"
+
+echo commiting changes
+git add --all
+git commit "Update Nightly"
+
+echo pushing to remote
 git push origin master

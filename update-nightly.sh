043f24ab00c4c1e943b324b178abf306a5a4b804
Andrew Holland <aholland@a1dutch.co.uk>
2014-12-18 13:42:34 +0000

    update nightly script

--- update-nightly.sh
+++ update-nightly.sh
@@ -1,20 +1,27 @@
 #! /bin/bash
 
-echo checking out website
+branch=$(git branch | awk '/^\*/{print $2}')
+
+if [ "$branch" != "master" ]; then 
+  echo Skipping update of nightly as on branch $branch
+  wxit
+fi 
+
+echo Cloning website
 git clone git@github.com:fitnesse-eclipse/fitnesse-eclipse.github.io.git
 cd fitnesse-eclipse.github.io
 git config user.email "fitnesseclipse@a1dutch.co.uk"
 git config user.name "fitnesseclipse"
 
-echo clearing nightly folder
+echo Clearing nightly folder
 rm -rf nightly/*
 
-echo updating nightly folder
+echo Updating nightly folder
 cp -r ~/fitnesse-eclipse/fitnesseclipse.site/target/repository/* nightly/
 
-echo commiting changes
+echo Commiting changes
 git add --all
 git commit -m "Update Nightly"
 
-echo pushing to remote
+echo Pushing to remote
 git push origin master
